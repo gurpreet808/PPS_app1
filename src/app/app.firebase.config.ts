e5230aa4be3d74227a5dee719d9a9ff135e0f0d5
Daniel Singh Montenegro <gurpreets808@gmail.com>
2018-09-21 22:58:13 -0300

    GitGuardian

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyBCCH8NAG8kdIZwRy-0YoQCg4FOw2-OrFA",
-    authDomain: "cargaqr.firebaseapp.com",
-    databaseURL: "https://cargaqr.firebaseio.com",
-    projectId: "cargaqr",
-    storageBucket: "cargaqr.appspot.com",
-    messagingSenderId: "609669593299"
-  };
