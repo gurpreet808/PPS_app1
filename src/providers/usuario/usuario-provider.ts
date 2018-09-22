047edbe79a4c941e72ce9698c29fa91533698619
gurpreet808 <gurpreets808@gmail.com>
2018-09-21 23:05:16 -0300

    test gitignore

--- src/providers/usuario/usuario-provider.ts
+++ src/providers/usuario/usuario-provider.ts
@@ -9,7 +9,7 @@ export class UsuarioProvider {
   user: User;
   access: boolean;
 
-  constructor(public afAuth: AngularFireAuth,private toastCtrl: ToastController) {
+  constructor(public afAuth: AngularFireAuth, private toastCtrl: ToastController) {
     afAuth.authState.subscribe(user => {
       this.user = user;
     });
@@ -20,8 +20,21 @@ export class UsuarioProvider {
     return this.afAuth.auth.signInWithEmailAndPassword(mail, pass);
   }
 
-  registrar(mail: string, pass: string) {
-    return this.afAuth.auth.createUserWithEmailAndPassword(mail, pass);
+  registrar(mail: string, pass: string, perfil: string, sexo: string) {
+    //return this.afAuth.auth.createUserWithEmailAndPassword(mail, pass);
+    if (true) {
+      
+    }
+  }
+
+  async registroAF(mail: string, pass: string){
+    try {
+      let result = await this.afAuth.auth.createUserWithEmailAndPassword(mail, pass);
+      console.log(result);
+    } catch (error) {
+      console.log(error);      
+    }
+
   }
 
   get authenticated(): boolean {
